d78613e846e139ab853efed7d27b611427f18d1d
Kent Collins <kent.r.collins@gmail.com>
2014-09-12 07:42:30 +0100

    Initial Commit

---
+++ BarnyardAnimal.java
@@ -0,0 +1,16 @@
+/*
+ * A common set of methods that all barnyard animals should
+ * implement.  If a class wishes to serve as a barnyard animal
+ * then it must (in some way) provide an implementation for
+ * each method.
+ * 
+ * @author Kent Collins
+ * @version 12 September, 2014
+ */
+public interface BarnyardAnimal {
+
+	public void soundOff();  // print your sound to the console
+	
+	public String toString();  // return a String description of yourself
+
+}
---
+++ BarnyardTester.java
@@ -0,0 +1,12 @@
+
+public class BarnyardTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
