e8fa970757976d40eb827a4d32b4c599ab658371
Kent Collins <kent.r.collins@gmail.com>
2014-09-26 23:07:25 +0100

    Update BarnyardAnimal.java

--- BarnyardAnimal.java
+++ BarnyardAnimal.java
@@ -1,15 +1,16 @@
 /*
- * A common set of methods that all barnyard animals should
- * implement.  If a class wishes to serve as a barnyard animal
- * then it must (in some way) provide an implementation for
- * each method.
+ * A common set of methods that establish what a BarnyardAnimal must do.
+ *
+ * Classes that implement this interface must provide an implementation for
+ * each method specified.
  * 
  * @author Kent Collins
  * @version 12 September, 2014
  */
+ 
 public interface BarnyardAnimal {
 
-	public void soundOff();  // print your sound to the console
+	public void greetFarmer();  // what does your animal say to the farmer? print that to the console.
 	
 	public String toString();  // return a String description of yourself
 
