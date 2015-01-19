b5563dafed46af101eb003f03d96c75a40113a09
Calvin Bohner <calvin_bohner@asl.org>
2015-01-19 09:07:21 +0000

    made Wolf

--- BarnyardTester.java
+++ BarnyardTester.java
@@ -11,6 +11,7 @@ public class BarnyardTester {
 	public static void main(String[] args) {
 		BarnyardAnimal[] animals = new BarnyardAnimal[10];
 		animals[0] = new Horse();
+		animals[1] = new Wolf();
 		for (BarnyardAnimal animal : animals) {
 			if (animal != null) {
 				System.out.println(animal.toString());
---
+++ Wolf.java
@@ -0,0 +1,15 @@
+
+public class Wolf implements BarnyardAnimal{
+public void greetFarmer(){
+	System.out.println("AAAHHHOOOOOOOO");
+	
+	
+}
+public String toString(){
+	
+	return "I am the night and I'll eat your sheep.";
+}
+	
+	
+	
+}
