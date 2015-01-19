60fd1e806428248c681be65ba6cdf382f30fe1c0
amnaha15 <amnaha15@asl.org>
2015-01-19 09:10:22 +0000

    Added Sheepdog

---
+++ Sheepdog.java
@@ -0,0 +1,12 @@
+
+public class Sheepdog {
+	@Override
+	public void greetFarmer() {
+		System.out.println("Woof! Woof");
+	}
+	
+	@Override
+	public String toString() {
+		return "The sheepdog is a responsible leader, manages the sheep very well.";
+	}
+}
