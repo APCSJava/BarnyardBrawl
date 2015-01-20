5a0c7c4a8f4cceb5da088eb4d4590367eec25201
sophiazorek <sophia_zorek@asl.org>
2015-01-20 09:08:19 +0000

    Shaggy Cow

---
+++ ShaggyCow.java
@@ -0,0 +1,24 @@
+/*
+	 * Implements the BarnyardAnimal interface in a manner befitting a horse.
+	 * 
+	 * @author Kent Collins
+	 * @version 14 September, 2014
+	 */
+
+public class ShaggyCow {
+	
+
+		@Override
+		public void greetFarmer() {
+			System.out.println("Moo");
+		}
+		
+		@Override
+		public String toString() {
+			return "The Shaggy Cow is majestic; it dominates the Scottish Highlands.";
+		}
+
+	}
+
+
+}
