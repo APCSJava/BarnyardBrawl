8bb275749393d756f23a7cc047bdcccee5072743
Sarah Brunsberg <sarah-brunsberg@users.noreply.github.com>
2015-01-22 14:40:57 +0000

    Update Goldfish.java
    No mistakes

--- Goldfish.java
+++ Goldfish.java
@@ -1,11 +1,18 @@
 /*
  * Implements the BarnyardAnimal interface in a manner befitting a Goldfish.
  * 
- * @author Kent Collins
- * @version 20 January, 2015
+ * @author Sarah Brunsberg
+ * @version 22 January, 2016
  */
  
-public class Horse implements BarnyardAnimal {
+public class Goldfish implements BarnyardAnimal {
+	
+	int blubsPerMinute;
+
+	public Goldfish(int blubsPerMinute){
+	  this.blubsPerMinute = blubsPerMinute;
+	}
+
 
 	@Override
 	public void greetFarmer() {
@@ -15,6 +22,12 @@ public class Horse implements BarnyardAnimal {
 	@Override
 	public String toString() {
 		return "Also called the dragons of the sea, Goldfish are known for their tough, unforgiving attitude.";
+	}
+	
+	public int compareTo(Goldfish c) {
+		  if (this.blubsPerMinute == c.blubsPerMinute) return 0;
+		  if (this.blubsPerMinute < c.blubsPerMinute) return -1;
+		  return 1;
 	}
 
 }
