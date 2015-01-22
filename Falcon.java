27c342150497d8f4ca2dc1be617ffc6a83f49bb2
vannc <christopher_vann@asl.org>
2015-01-22 14:44:23 +0000

    FAST FALCON
    Added MAXVELOCITY field and implemented compare to Falcon class

--- Falcon.java
+++ Falcon.java
@@ -4,17 +4,30 @@
  * @author Christopher Vann
  * @version 20 January, 2015
  */
- 
+
 public class Falcon implements BarnyardAnimal {
-
- @Override
- public void greetFarmer() {
-  System.out.println("Skreeee!");
+  
+  int MAXVELOCITY;
+  
+   public Falcon(int MAXVELOCITY){
+   this.MAXVELOCITY = MAXVELOCITY;
  }
- 
- @Override
- public String toString() {
-  return "The falcon is lithe and deadly, the fastest animal in the world.";
- }
+  
+  @Override
+  public void greetFarmer() {
+    System.out.println("Skreeee!");
+  }
+  
+  @Override
+  public String toString() {
+    return "The falcon is lithe and deadly, the fastest animal in the world.";
+  }
+  
 
+ // Java 1.7 and beyond
+ public int compareTo(Falcon f) {
+    if (this.MAXVELOCITY == f.MAXVELOCITY) return 0;
+    if (this.MAXVELOCITY < f.MAXVELOCITY) return -1;
+    return 1;
+ }
 }
---
+++ FalconTester.java
@@ -0,0 +1,15 @@
+/**
+ * Auto Generated Java Class.
+ */
+public class FalconTester {
+  
+  
+  public static void main(String[] args) { 
+    Falcon peregrin = new Falcon(20);
+    Falcon black = new Falcon(20);
+    System.out.println(peregrin.compareTo(black));
+  }
+  
+  /* ADD YOUR CODE HERE */
+  
+}
