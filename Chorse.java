a265e0bebef9691b83ecb283578ea4f6e72f24f0
Kent Collins <kent_collins@asl.org>
2015-01-22 10:53:55 +0000

    updated

--- Chorse.java
+++ Chorse.java
@@ -1,5 +1,7 @@
 /*
- * Implements the BarnyardAnimal interface in a manner befitting a horse.
+ * Implements both the BarnyardAnimal interface 
+ * and the Comparable interface in a manner befitting 
+ * a horse.
  * 
  * @author Kent Collins
  * @version 14 September, 2014
@@ -23,10 +25,15 @@ public class Chorse implements BarnyardAnimal, Comparable {
 		return "The horse is sturdy and powerful, capable of much work.";
 	}
 
+	/**
+	 * @param o An object to be compared to this horse
+	 * 
+	 * @return -1 if this horse is shorter, 0 if same, +1 if this horse taller
+	 */
 	public int compareTo(Object o) {
 		  Chorse other = (Chorse) o;
 		  if (this.height == other.height) return 0;
-		  if (this.height <= other.height) return -1;
+		  if (this.height < other.height) return -1;
 		  return 1;
 	}
 }
