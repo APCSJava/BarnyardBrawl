fb2803f5f2992f16d42fba1ad26cf9c3611e6e29
Tristan Burke <tristan_burke@asl.org>
2015-01-22 14:46:01 +0000

    Update MamaHen.java

--- MamaHen.java
+++ MamaHen.java
@@ -1,13 +1,22 @@
-public class MamaHen implements BarnyardAnimal {
-
- @Override
- public void greetFarmer() {
+public class MamaHen implements BarnyardAnimal, Comparable {
+int weight;  
+@Override
+public void greetFarmer() {
   System.out.println("Sup Farmer");
  }
- 
- @Override
- public String toString() {
-  return "Mama Hen is the queen of the farm";
- }
+public MamaHen(int weight){
+    this.weight = weight;
+}
+@Override
+public String toString() 
+{
+return "Mama Hen is the queen of the farm";
+}
 
+public int compareTo(Object o) {
+    MamaHen other = (MamaHen) o;
+    if (this.weight == other.weight) return 0;
+    if (this.weight < other.weight) return -1;
+    return 1;
+ }
 }
