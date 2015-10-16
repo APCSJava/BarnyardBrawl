4a4281eea47056b4e532f152de4f78636712c7f0
Kent Collins <kent.r.collins@gmail.com>
2015-10-16 14:48:21 +0100

    filled the barnyard

--- BarnyardTester.java
+++ BarnyardTester.java
@@ -11,6 +11,15 @@ public class BarnyardTester {
 	public static void main(String[] args) {
 		BarnyardAnimal[] animals = new BarnyardAnimal[10];
 		animals[0] = new Horse();
+		animals[1] = new Doge();
+		animals[2] = new Owl();
+		animals[3] = new TheVoid();
+		animals[4] = new Dinosaur();
+		animals[5] = new DapperFellow();
+		animals[6] = new Chicken();
+		animals[7] = new JohnCena();
+		animals[8] = new WoolyPig();
+		animals[9] = new HoneyBadger();
 
 		for (BarnyardAnimal animal : animals) {
 			if (animal != null) {
