666687e4240fa55bc1d514b961c077a6d9776dfa
Callum Joseph Hafner Schnee <CallumHafnerSchnee@theschnee.local>
2015-10-19 09:28:35 +0100

    Here's Godzilla

---
+++ Godzillaa.java
@@ -0,0 +1,12 @@
+
+public class Godzillaa implements BarnyardAnimal {
+
+	@Override
+	public void greetFarmer() {
+		System.out.println("Moo.")
+		System.out.println("Actually, uhrlrahrrrgrraa")
+		// TODO Auto-generated method stub
+
+	}
+
+}
