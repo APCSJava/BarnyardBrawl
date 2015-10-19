68e4db8902b783569cb7d279cd0bcf3c7aa402aa
Kent Collins <kentc14@hsfac_19365.local>
2015-10-19 09:52:30 +0100

    Duck

--- Duck.java
+++ Duck.java
@@ -1,4 +1,14 @@
 
-public class Duck {
+public class Duck implements BarnyardAnimal {
+
+	@Override
+	public void greetFarmer() {
+		System.out.println("Quack!");
+		
+	}
+	
+	public String toString(){
+		return "The duck is a little quackers";
+	}
 
 }
