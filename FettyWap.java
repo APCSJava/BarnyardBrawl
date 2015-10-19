5b883c3f4ab493d15eaa6aed729597569cf43f88
Neil Meehan <Neil@Neils-MacBook-Pro.local>
2015-10-19 09:15:47 +0100

    fetty wap is here

---
+++ FettyWap.java
@@ -0,0 +1,18 @@
+public class FettyWap implements BarnyardAnimal {
+
+	public FettyWap() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void greetFarmer() {
+		System.out.println("Hey! What's up? Hello!");
+	}
+	
+	@Override
+	public String toString() {
+		 return "This Fetty Wap has great vision and will be a great asset to you on the farm";
+	}
+	
+
+}
