3a7c90fd632b8ece1ac6f2bdbfd41d8a0248f32e
Emmetk17 <EmmetsMac@Emmets-MacBook-Pro.local>
2015-10-19 09:13:31 +0100

    added the shrew

---
+++ Shrew.java
@@ -0,0 +1,12 @@
+public class Shrew implements BarnyardAnimal {
+
+	@Override
+	public void greetFarmer() {
+		System.out.println("Squeeeek!");
+	}
+	@Override
+	public String toString() {
+		return "The shrew is tiny, and full of energy.";
+	}
+
+}
