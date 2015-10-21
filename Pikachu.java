83732414979b00a49051545ea0445d9bc86a33ff
David Costin <David@Davids-MacBook-Pro-2.local>
2015-10-21 13:17:36 +0100

    e Please enter the commit message for your changes. Lines starting

--- Pikachu.java
+++ Pikachu.java
@@ -1,4 +1,11 @@
 
-public class Pikachu {
-
+public class Pikachu implements BarnyardAnimal{
+	
+	public void greetFarmer() {
+		System.out.println("Pika Pika CHUUUUUUUUUU!!!");
+	}
+	
+	public String toString() {
+		return "Pikachu is a small yellow electic type rat pokemon.";
+	}
 }
