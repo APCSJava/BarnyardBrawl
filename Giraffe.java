94396c55efe609d2569412b0e3175472775101df
gblack22 <gabrielle_black@asl.org>
2015-10-21 12:32:19 +0000

    Giraffe

---
+++ Giraffe.java
@@ -0,0 +1,10 @@
+public class Giraffe implements BarnyardAnimal {
+	@Override
+	public void greetFarmer() {
+		System.out.println("What even");
+		
+	}
+	public String toString() {
+		return "The Giraffe is too tall, it's unable to get inside the barn";
+	}
+}
